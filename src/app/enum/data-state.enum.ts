f117864e89596d2c058f65bf00a8630273345361
noe <noe.reyes92@yahoo.com>
2022-02-24 15:37:41 -0600

    going to attempt to work with state of application - created customer-response and data-state.enum files

---
+++ src/app/enum/data-state.enum.ts
@@ -0,0 +1,6 @@
+// Refers to the state of the application
+export enum DataState {
+    LOADING = 'LOADING',
+    LOADED = 'LOADED',
+    ERROR = 'ERROR'
+}
--- src/app/interface/service.ts
+++ src/app/interface/service.ts
@@ -1,5 +1,8 @@
+import { Status } from "../enum/status.enum";
+
 export interface Server {
     id: number;
     body: string;
     completed: boolean;
+    status: Status;
 }
