f0bd4d96b527744baff4c30854143bd30e6030a1
noe <noe.reyes92@yahoo.com>
2022-02-24 15:29:23 -0600

    created an enum and interface folder

---
+++ src/app/enum/status.enum.ts
@@ -0,0 +1,5 @@
+export enum Status {
+    ALL = 'ALL',
+    SERVER_UP = 'SERVER-UP',
+    SERVER_DOWN = 'SERVER_DOWN'
+}
---
+++ src/app/interface/service.ts
@@ -0,0 +1,5 @@
+export interface Server {
+    id: number;
+    body: string;
+    completed: boolean;
+}
