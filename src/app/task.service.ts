f601e7b23758b0e82ffc83af46d222cab1bcd7bf
noe <noe.reyes92@yahoo.com>
2022-02-26 19:33:21 -0600

    created task interface

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
+import { Task } from './task';
 
 @Injectable({
   providedIn: 'root'
