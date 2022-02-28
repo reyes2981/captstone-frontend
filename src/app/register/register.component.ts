b9ad4ae60cccf76c830e700e78188ab5125dc067
noe <noe.reyes92@yahoo.com>
2022-02-28 17:44:06 -0600

    created a register.service.ts file

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  user: User = new User();
+
   constructor() { }
+
+  userRegister() {
+    console.log(this.user);
+  }
 
   ngOnInit(): void {
   }
