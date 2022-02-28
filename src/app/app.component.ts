4a6437ffe299716b092f3f05fef673616d0ff077
noe <noe.reyes92@yahoo.com>
2022-02-27 21:55:27 -0600

    added basic logic to onClick method in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'frontend';
 
-
+  display = false;
   public onClick() {
+    console.log("clicked");
     
+    this.display = true;
   }
   
 
 }
+
