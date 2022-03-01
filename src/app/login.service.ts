9451587aa50cd31649696b45d1a59ed2de03cbb5
noe <noe.reyes92@yahoo.com>
2022-02-28 19:13:06 -0600

    created loginuser method in login.service.ts and added initial logic

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,9 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  baseUrl = "http://localhost:9092/login";
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
+
+  loginUser(user: User): Observable<Object>{
+    return this.http.post();
+  }
+
 }
