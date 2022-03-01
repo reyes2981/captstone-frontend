6c30f42eaed3f618407f54d9e45f48ed6c646ffc
noe <noe.reyes92@yahoo.com>
2022-02-28 18:22:19 -0600

    registration form is now functional

--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  baseUrl = "http://localhost:9092/registration";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registerUser(user: User): Observable<Object> {
+    console.log(user);
+    return this.http.post(`${this.baseUrl}`, user);
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../register.service';
 import { User } from '../user';
 
 @Component({
@@ -10,10 +11,16 @@ export class RegisterComponent implements OnInit {
 
   user: User = new User();
 
-  constructor() { }
+  constructor(private registerService: RegisterService) {
+
+  }
 
   userRegister() {
     console.log(this.user);
+    this.registerService.registerUser(this.user).subscribe(data => {
+      alert("User is registered!")
+    },
+    error => alert("User was not registered.")); 
   }
 
   ngOnInit(): void {
