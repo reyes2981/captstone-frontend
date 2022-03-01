182bc2c6917dd87bebbca280f3eb201e1ffa8435
noe <noe.reyes92@yahoo.com>
2022-03-01 17:23:39 -0600

    added table to user list html

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
+  users: User[];
+  
   constructor() { }
 
   ngOnInit(): void {
