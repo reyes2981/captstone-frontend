353eb09b15c7402d492c6ec152838f8fcd222bd6
noe <noe.reyes92@yahoo.com>
2022-03-01 18:29:50 -0600

    added adduser link to nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
+import { CreateUserComponent } from "./create-user/create-user.component";
 import { HomeComponent } from "./home/home.component";
 import { NavComponent } from "./nav/nav.component";
 import { TaskComponent } from "./task/task.component";
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'users', pathMatch: 'full'
+  },
+  {
+    path: 'create-user',
+    component: CreateUserComponent
   },
   {
     path: 'users',
