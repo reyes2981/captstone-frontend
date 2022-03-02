1f67ae150090ac95c9253fd16d2f22a4417aefcd
noe <noe.reyes92@yahoo.com>
2022-03-01 18:01:06 -0600

    added navigation functionality for task component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import { HomeComponent } from "./home/home.component";
 import { NavComponent } from "./nav/nav.component";
+import { TaskComponent } from "./task/task.component";
 import { UserListComponent } from "./user-list/user-list.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users', pathMatch: 'full'
+  },
   {
     path: 'users',
     component: UserListComponent
   },
   {
-    path: 'home',
-    component: HomeComponent
+    path: 'task',
+    component: TaskComponent
   },
   
 
